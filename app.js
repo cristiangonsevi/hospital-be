9cd2502b5e40df3c3d18192cc1c08ebade32acff
Cristian Gonzalez Sevillano <cgonzalez@pop-os.localdomain>
2021-05-25 23:24:04 -0600

    remove/ Removed example html file for google auth

--- app.js
+++ app.js
@@ -11,10 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 dbConnection();
-app.get('/', (req, res) => {
-    const path = '/home/cgonzalez/Desktop/index.html';
-    res.sendFile(path)
-});
+
 app.use('/v1', router);
 
 module.exports = { app };
